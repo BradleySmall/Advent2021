cfc5c9c1b797ea246af3ca2acf04276e16375c30
Bradley M. Small <bradley_small@hotamil.com>
2021-12-07 11:53:44 -0500

    day7 second star

---
+++ day7/src/test/groovy/com/small/advent2021/day7/Crabs_Specification.groovy
@@ -0,0 +1,20 @@
+package com.small.advent2021.day7
+
+import spock.lang.Specification
+
+class Crabs_Specification extends Specification {
+    private Crabs crabs
+
+    void setup() {
+        crabs = new Crabs("input_test.txt")
+    }
+
+    def "should show min and max fuelUsage"() {
+        when:
+        crabs.calculateFuelUsage()
+
+        then:
+        crabs.getMinFuelUsage() == 168
+        crabs.getMaxFuelUsage() == 817
+    }
+}
