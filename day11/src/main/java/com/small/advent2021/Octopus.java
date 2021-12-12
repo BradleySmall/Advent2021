372f0e49d3692846dad18befc2193543cdd41ba9
Bradley M. Small <bradley_small@hotamil.com>
2021-12-12 14:21:56 -0500

    Day 11 - completed both stars

--- day11/src/main/java/com/small/advent2021/Octopus.java
+++ day11/src/main/java/com/small/advent2021/Octopus.java
@@ -1,19 +1,19 @@
 package com.small.advent2021;
 
-import groovy.lang.Tuple;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 import java.util.stream.Stream;
+
+import static java.lang.String.format;
 
 @Slf4j
 public class Octopus {
-    int [][] matrix;
+    int[][] matrix;
+    private int flashes;
 
     public Octopus(String fileName) {
         processFile(fileName);
@@ -21,14 +21,14 @@ public class Octopus {
 
     private void processFile(String fileName) {
         Path path = Path.of(fileName);
-        try(Stream<String> s = Files.lines(path)) {
-           matrix =  s
+        try (Stream<String> s = Files.lines(path)) {
+            matrix = s
                     .map(str -> str.chars()
-                                    .map(ch -> ch - '0')
+                            .map(ch -> ch - '0')
                             .toArray()
                     )
                     .toArray(int[][]::new)
-                    ;
+            ;
         } catch (IOException e) {
             log.info(e.getMessage());
             e.printStackTrace();
@@ -36,80 +36,68 @@ public class Octopus {
     }
 
     void passStep() {
-        for (int row = 0; row < 10;++row) {
-            for(int column=0;column < 10;++column) {
-                int tmp = matrix[row][column];
-                tmp =  tmp + 1;
-                matrix[row][column] = tmp;
+        for (int row = 0; row < matrix.length; ++row) {
+            for (int column = 0; column < matrix[0].length; ++column) {
+                ++matrix[row][column];
             }
         }
-        //log.info("after pass\n"+show());
 
-        while(show().contains("10")) {
-            for (int row = 0; row < 10; ++row) {
-                for (int column = 0; column < 10; ++column) {
-                    int tmp = matrix[row][column];
-                    if (tmp == 10) {
+        while (show().contains("10")) {
+            for (int row = 0; row < matrix.length; ++row) {
+                for (int column = 0; column < matrix[0].length; ++column) {
+                    if (matrix[row][column] == 10) {
                         flashTheNeighbors(row, column);
                     }
                 }
             }
         }
-        //log.info("After Flash\n"+show());
-        for (int r = 0; r < 10; ++r) {
-            for (int c = 0; c < 10; ++c) {
-                int tmp = matrix[r][c];
-                if (tmp >= 10) {
-                    tmp = 0;
-                }
-                matrix[r][c]=tmp;
-            }
-        }
-        log.info("After Flash\n"+show());
+        log.debug("After Step\n" + show());
 
     }
 
     private void flashTheNeighbors(int row, int column) {
-        int beginRow = (row > 0) ? row -1 : row;
-        int beginColumn = (column > 0) ? column-1 : column;
-        int endRow = (row < 9) ? row + 1 : 9;
-        int endColumn = (column < 9) ? column + 1 : 9;
+        int beginRow = (row > 0) ? row - 1 : row;
+        int beginColumn = (column > 0) ? column - 1 : column;
+        int endRow = (row < matrix.length - 1) ? row + 1 : matrix.length - 1;
+        int endColumn = (column < matrix[0].length - 1) ? column + 1 : matrix[0].length - 1;
 
-
-            for (int r = beginRow; r <= endRow; ++r) {
-                for (int c = beginColumn; c <= endColumn; ++c) {
-                    int tmp = matrix[r][c];
-
-                    tmp = tmp+1;
-                    matrix[r][c] = tmp;
+        for (int r = beginRow; r <= endRow; ++r) {
+            for (int c = beginColumn; c <= endColumn; ++c) {
+                if (matrix[r][c] != 0 && matrix[r][c] != 10) {
+                    ++matrix[r][c];
                 }
             }
+        }
+        matrix[row][column] = 0;
+        ++flashes;
     }
 
     public String show() {
         return Arrays
                 .deepToString(matrix)
-                .replace("[[","\n [")
-                .replace("],","]\n")
-                .replace("]]","]\n");
+                .replace("[[", "\n [")
+                .replace("],", "]\n")
+                .replace("]]", "]\n");
+    }
+
+    public boolean allFlashed() {
+        return Arrays
+                .stream(matrix)
+                .flatMapToInt(Arrays::stream)
+                .allMatch(power -> power == 0);
     }
 
     public static void main(String[] args) {
-        Octopus octopus = new Octopus("day11/input_test.txt");
-        //log.info(octopus.show());
-        octopus.passStep();
-        //log.info(octopus.show());
-        octopus.passStep();
-        //log.info(octopus.show());
-        octopus.passStep();
-        //log.info(octopus.show());
-        octopus.passStep();
-        //log.info(octopus.show());
-        octopus.passStep();
-        //log.info(octopus.show());
-        octopus.passStep();
-        //log.info(octopus.show());
-        octopus.passStep();
-        //log.info(octopus.show());
+        Octopus octopus = new Octopus("day11/input.txt");
+
+        int step;
+        for (step = 0; !octopus.allFlashed(); step++) {
+            octopus.passStep();
+            log.debug(format("All Flashed? %b", octopus.allFlashed()));
+            if (step == 99) {
+                log.info(format("After Step %d %n%s%nFlashes=%d", step+1, octopus.show(), octopus.flashes));
+            }
+        }
+        log.info(format("After Step %d %n%s%n", step, octopus.show()));
     }
 }
