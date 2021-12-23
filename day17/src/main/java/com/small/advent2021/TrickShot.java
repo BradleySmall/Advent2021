d4f41bc59694dc7c42fdd360ced13941cca6a4bb
Bradley M. Small <bradley_small@hotmail.com>
2021-12-23 18:23:17 -0500

    Day17 - Both stars

--- day17/src/main/java/com/small/advent2021/TrickShot.java
+++ day17/src/main/java/com/small/advent2021/TrickShot.java
@@ -5,12 +5,120 @@ import lombok.extern.slf4j.Slf4j;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.lang.String.format;
 
 @Slf4j
 public class TrickShot {
+    List<String> list = new ArrayList<>();
     TrickShot(String fileName) {
         processFile(fileName);
 
+    }
+
+
+    private class Trajectory {
+        int x;
+        int y;
+
+        int xInc;
+        int yInc;
+
+        void setVelocity(int xVelocity, int yVelocity) {
+            xInc = xVelocity;
+            yInc = yVelocity;
+            x = 0;
+            y = 0;
+        }
+
+        void step() {
+            x += xInc;
+            y += yInc;
+
+            if (xInc > 0) {
+                --xInc;
+            }
+            --yInc;
+        }
+
+        boolean inRange(int xBeginRange, int yTop, int xEndRange, int yBottom) {
+            while (true) {
+                step();
+                if (x >= xBeginRange && x <= xEndRange && y <= yTop && y >= yBottom) {
+                    list.add(String.format("(%d,%d)", x, y));
+                    return true;
+                }
+                if (x > xEndRange || y < yBottom) {
+                    return false;
+                }
+            }
+        }
+
+    }
+
+    int countValidStarts(int xBeginRange, int xEndRange, int yTop, int yBottom) {
+        Trajectory trajectory = new Trajectory();
+        int count = 0;
+        for (int x = 24; x <= xEndRange; ++x) {
+            for (int y = 80; y >= -80; --y) {
+                trajectory.setVelocity(x, y);
+                if(trajectory.inRange(xBeginRange, yTop, xEndRange, yBottom)) {
+                    log.info(format("Valid Trajectory (%d, %d)", x, y));
+                    ++count;
+                }
+            }
+        }
+        return count;
+    }
+
+    int vert(int vel, int rangeTop, int rangeBottom) {
+        int x = vel;
+        int pos = 0;
+        for (; x > 0; --x) {
+            pos += x;
+            log.info("^V:" + pos);
+
+        }
+
+        int apogee = pos;
+
+        for (; pos >= rangeBottom; x++) {
+            pos -= x;
+            log.info("V:" + pos);
+            if (pos < rangeTop) {
+                break;
+            }
+        }
+
+        return apogee;
+    }
+
+    int func(int velocity, int rangeBegin, int rangeEnd) {
+        int pos = 0;
+
+        for (int x = velocity; x >= 0; --x) {
+            pos += x;
+            log.debug("H:" + pos);
+            if (pos > rangeBegin) {
+                break;
+            }
+        }
+        if (pos >= rangeBegin && pos <= rangeEnd) {
+            return pos;
+        } else {
+            return 0;
+        }
+    }
+
+    int findH(int rangeBegin, int rangeEnd) {
+        for (int velocity = 0; velocity < rangeEnd; ++velocity) {
+            if (0 != func(velocity, rangeBegin, rangeEnd)) {
+                log.info("Valid Velocity: " + velocity);
+            }
+        }
+        return 7;
     }
 
     private void processFile(String fileName) {
--- day17/src/test/groovy/com/small/advent2021/TrickShotTest.groovy
+++ day17/src/test/groovy/com/small/advent2021/TrickShotTest.groovy
@@ -1,15 +1,23 @@
 package com.small.advent2021
 
-import spock.lang.Ignore
 import spock.lang.Specification
 
 class TrickShotTest extends Specification {
-    @Ignore
+
+    def "should return apogee for 79"() {
+        given:
+        TrickShot trickShot = new TrickShot("input.txt")
+
+        expect:
+        3160 == trickShot.vert(79, -45, -80)
+
+    }
     def "should"() {
         given:
         TrickShot trickShot = new TrickShot("input.txt")
 
         expect:
-        false
+        7 == trickShot.findH(282, 314)
+        1928 == trickShot.countValidStarts(282, 314, -45, -80)
     }
 }
