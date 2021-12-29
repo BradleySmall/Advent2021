ef533721ec955b15a1fd36aa368ac44a1a01c490
Bradley M. Small <bradley_small@hotamil.com>
2021-12-29 14:59:32 -0500

    Starting to make lists work, but the code is too complex to do it cleanly. Checking in to not lose any work.

--- day18/src/main/java/com/small/advent2021/SnailFish.java
+++ day18/src/main/java/com/small/advent2021/SnailFish.java
@@ -4,7 +4,10 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.util.ArrayDeque;
 import java.util.Deque;
+import java.util.List;
 import java.util.Objects;
+
+import static java.lang.String.format;
 
 @Slf4j
 public class SnailFish {
@@ -29,9 +32,17 @@ public class SnailFish {
         }
 
         public Number add(Number n) {
-            return new Number(String.format("[%s,%s]", str, n.toString()));
+            if (this.str.equals("")) return n;
+            return new Number(format("[%s,%s]", str, n.toString()));
         }
+        public Number addList(List<Number> list) {
+            Number n = new Number("");
+            for (var a : list) {
+                n = n.add(a);
+            }
 
+            return n;
+        }
         @Override
         public String toString() {
             return str;
@@ -61,6 +72,7 @@ public class SnailFish {
             String closeBrackets = "";
             String number;
             String addNumber = "";
+            boolean done = false;
 
             for(var a : str.split(",")) {
                 if(a.charAt(0) == '[') {
@@ -74,8 +86,12 @@ public class SnailFish {
                 }
 
                 depth += pushBrackets(openBrackets, stack);
-                if (depth > 4) {
 
+                if (done) {
+                    depth = 0;
+                }
+                if (depth > 4) {
+                    done = true;
                     explode = true;
                     stack.pop();
                     --depth;
@@ -86,7 +102,7 @@ public class SnailFish {
                         } else {
                             stack.pop();
                             addNumber = stack.pop();
-                            number = String.format("%d", Integer.parseInt(addNumber) + Integer.parseInt(number));
+                            number = format("%d", Integer.parseInt(addNumber) + Integer.parseInt(number));
                             addNumber = "";
                         }
                     }
@@ -99,7 +115,9 @@ public class SnailFish {
                             number= "0";
                         } else {
                             closeBrackets = "";
+//                            number = "";
                         }
+//                        if (openBrackets.length() > 0) stack.pop();
 
                         --depth;
                     } else if (!addNumber.isEmpty()) {
@@ -107,19 +125,23 @@ public class SnailFish {
                         String s2 = stack.pop();
 
                         if(s2.matches("[0-9]+")) {
-                            number = String.format("%d", Integer.parseInt(s2) + Integer.parseInt(number));
+                            number = format("%d", Integer.parseInt(s2) + Integer.parseInt(number));
                         } else {
                             stack.push(s2);
                             stack.push(s1);
-                            number = String.format("%d", Integer.parseInt(addNumber) + Integer.parseInt(number));
+                            number = format("%d", Integer.parseInt(addNumber) + Integer.parseInt(number));
                         }
 
                         addNumber = "";
                     }
                 }
-                stack.push(number);
+                if (!number.equals("")) {
+                    stack.push(number);
+                }
                 depth -= pushBrackets(closeBrackets, stack);
-                stack.push(",");
+                if (!number.equals("")) {
+                    stack.push(",");
+                }
 
                 openBrackets = "";
                 closeBrackets = "";
@@ -136,6 +158,64 @@ public class SnailFish {
                 ++depth;
             }
             return depth;
+        }
+
+        public Number splitReduce() {
+            StringBuilder sb = new StringBuilder();
+            String openBrackets = "";
+            String closeBrackets = "";
+            String number;
+            boolean done = false;
+
+            for(var a : str.split(",")) {
+                if (a.charAt(0) == '[') {
+                    int idx = a.lastIndexOf('[') + 1;
+                    openBrackets = a.substring(0, idx);
+                    number = a.substring(idx);
+                } else {
+                    int idx = a.indexOf(']');
+                    number = a.substring(0, idx);
+                    closeBrackets = a.substring(idx);
+                }
+
+                if (!openBrackets.isEmpty()) sb.append(openBrackets);
+                if (!done) {
+                    var t = getNumberOrSplit(number);
+                    if (!t.equals(number)) {
+                        done = true;
+                        number = t;
+                    }
+                }
+                sb.append(number);
+                if (!closeBrackets.isEmpty()) sb.append(closeBrackets);
+                sb.append(",");
+                openBrackets = "";
+                closeBrackets = "";
+            }
+            sb.deleteCharAt(sb.length()-1);
+            return new Number(sb.toString());
+        }
+
+        private String getNumberOrSplit(String number) {
+            StringBuilder sb = new StringBuilder();
+            int num = Integer.parseInt(number);
+            if (num > 9) {
+                int leftNum;
+                int rightNum;
+                if (num % 2 == 0) {
+                   leftNum = num / 2;
+                   rightNum = leftNum;
+                } else {
+                    leftNum = num / 2;
+                    rightNum = leftNum + 1;
+                }
+                sb.append(format("[%d,%d]",leftNum, rightNum));
+            } else {
+                sb.append(number);
+            }
+
+
+            return sb.toString();
         }
     }
 
--- day18/src/test/groovy/com/small/advent2021/SnailFishTest.groovy
+++ day18/src/test/groovy/com/small/advent2021/SnailFishTest.groovy
@@ -10,6 +10,70 @@ class SnailFishTest extends Specification {
         snailFish = new SnailFish()
     }
 
+    def "should add list example 3"() {
+        given:
+        var theList = List.of(
+                new SnailFish.Number("[1,1]"),
+                new SnailFish.Number("[2,2]"),
+                new SnailFish.Number("[3,3]"),
+                new SnailFish.Number("[4,4]"),
+                new SnailFish.Number("[5,5]"),
+                new SnailFish.Number("[6,6]")
+        )
+        SnailFish.Number n1 = new SnailFish.Number("").addList(theList)
+        SnailFish.Number n2 = new SnailFish.Number("[[[[5,0],[7,4]],[5,5]],[6,6]]")
+
+        n1 = n1.reduce()
+        n1 = n1.reduce()
+        expect:
+        n2 == n1
+
+    }
+    def "should add list example 2"() {
+        given:
+        var theList = List.of(
+                new SnailFish.Number("[1,1]"),
+                new SnailFish.Number("[2,2]"),
+                new SnailFish.Number("[3,3]"),
+                new SnailFish.Number("[4,4]"),
+                new SnailFish.Number("[5,5]")
+        )
+        SnailFish.Number n1 = new SnailFish.Number("").addList(theList)
+        SnailFish.Number n2 = new SnailFish.Number("[[[[3,0],[5,3]],[4,4]],[5,5]]")
+
+        n1 = n1.reduce().reduce()
+        expect:
+        n2 == n1
+
+    }
+    def "should add list"() {
+        given:
+        var theList = List.of(
+                new SnailFish.Number("[1,1]"),
+                new SnailFish.Number("[2,2]"),
+                new SnailFish.Number("[3,3]"),
+                new SnailFish.Number("[4,4]"))
+        SnailFish.Number n1 = new SnailFish.Number("").addList(theList)
+        SnailFish.Number n2 = new SnailFish.Number("[[[[1,1],[2,2]],[3,3]],[4,4]]")
+
+        expect:
+        n2 == n1
+
+    }
+
+    def "should split first example"() {
+        given:
+        SnailFish.Number n1 = new SnailFish.Number("[[[[0,7],4],[15,[0,13]]],[1,1]]")
+        SnailFish.Number n2 = new SnailFish.Number("[[[[0,7],4],[[7,8],[0,13]]],[1,1]]")
+        SnailFish.Number n3 = new SnailFish.Number("[[[[0,7],4],[[7,8],[0,[6,7]]]],[1,1]]")
+        SnailFish.Number n4 = new SnailFish.Number("[[[[0,7],4],[[7,8],[6,0]]],[8,1]]")
+
+        expect:
+        n2 == n1.splitReduce()
+        n3 == n2.splitReduce()
+        n4 == n3.reduce()
+    }
+
     def "should reduce fifth example number"() {
 
         given:
@@ -30,6 +94,7 @@ class SnailFishTest extends Specification {
         expect:
         n2 == n1.reduce()
     }
+
     def "should reduce a third number"() {
         given:
         SnailFish.Number n1 = new SnailFish.Number("[[6,[5,[4,[3,2]]]],1]")
@@ -39,6 +104,7 @@ class SnailFishTest extends Specification {
         expect:
         n2 == n1.reduce()
     }
+
     def "should reduce a second number"() {
         given:
         SnailFish.Number n1 = new SnailFish.Number("[7,[6,[5,[4,[3,2]]]]]")
@@ -48,6 +114,7 @@ class SnailFishTest extends Specification {
         expect:
         n2 == n1.reduce()
     }
+
     def "should reduce a number"() {
         given:
         SnailFish.Number n1 = new SnailFish.Number("[[[[[9,8],1],2],3],4]")
@@ -57,6 +124,7 @@ class SnailFishTest extends Specification {
         expect:
         n2 == n1.reduce()
     }
+
     def "should add 2 numbers"() {
         given:
         SnailFish.Number n1 = new SnailFish.Number("[1,2]")
