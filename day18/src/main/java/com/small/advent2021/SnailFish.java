8a24f06c73ade92de4132b1917c55127d52b058d
Bradley M. Small <bradley_small@hotamil.com>
2021-12-29 00:02:40 -0500

    SnailFish work in progress. No starts, but some UTs pass.

--- day18/src/main/java/com/small/advent2021/SnailFish.java
+++ day18/src/main/java/com/small/advent2021/SnailFish.java
@@ -2,6 +2,136 @@ package com.small.advent2021;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Objects;
+
 @Slf4j
 public class SnailFish {
+
+
+    public static class Number {
+        private final String str;
+
+        public Number(String str) {
+            this.str = str;
+        }
+        public Number add(Number n) {
+            return new Number(String.format("[%s,%s]", str, n.toString()));
+        }
+
+        @Override
+        public String toString() {
+            return str;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Number number = (Number) o;
+            return Objects.equals(str, number.str);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(str);
+        }
+
+        public Number reduce() {
+            StringBuilder sb = new StringBuilder();
+            Deque<String> stack = new ArrayDeque<>();
+
+            int depth = 0;
+            boolean addNext = false;
+
+            for(var a : str.split(",")) {
+                String brackets;
+                String num;
+                String addNum = null;
+                if(a.charAt(0) == '[') {
+                    int idx = a.lastIndexOf('[') + 1;
+                    String theBkts = a.substring(0,idx);
+                    String theNum = a.substring(idx);
+
+                    if(addNext == true) {
+                        String comma = stack.pop();
+                        String bkts = stack.pop();
+                        addNum = stack.pop();
+                        stack.push("0");
+                        stack.push(bkts.substring(1));
+                        stack.push(comma);
+                        addNext = false;
+                    }
+
+
+                    depth += idx;
+                    if (depth > 4) {
+                        brackets = a.substring(1, idx);
+                        if (stack.peek() == null || stack.peek().contains("[")) {
+                            num = "0";
+                        } else {
+                            stack.pop();
+                            var tmp = stack.pop();
+                            num = String.format("%d", Integer.parseInt(tmp) + Integer.parseInt(a.substring(idx)));
+                        }
+                        addNext = true;
+                    } else {
+                        brackets = a.substring(0, idx);
+                        if (addNum != null)
+                            num = String.format("%d", Integer.parseInt(addNum) + Integer.parseInt(a.substring(idx)));
+                        num = a.substring(idx);
+                    }
+                    stack.push(brackets);
+                    stack.push(num);
+                } else {
+                    int idx = a.indexOf(']');
+
+                    if (depth > 4) {
+                        brackets = a.substring(idx);
+                        num = a.substring(0,idx);
+                        stack.push(num);
+                        stack.push(brackets);
+                        addNext = true;
+                    } else if (addNext){
+                        String comma = stack.pop();
+                        String bkts = stack.pop();
+                        var tmp = stack.pop();
+                        stack.push("0");
+                        stack.push(bkts.substring(1));
+                        stack.push(comma);
+
+                        num = String.format("%d", Integer.parseInt(tmp) + Integer.parseInt(a.substring(0,idx)));
+                        brackets = a.substring(idx);
+                        stack.push(num);
+                        stack.push(brackets);
+                        addNext = false;
+                    } else {
+                        num = a.substring(0,idx);
+                        brackets = a.substring(idx);
+                        stack.push(num);
+                        stack.push(brackets);
+                    }
+                    depth -= a.length() - idx;
+                }
+                stack.push(",");
+            }
+            if(addNext == true) {
+                String s1 = stack.pop();
+                String s2 = stack.pop();
+                String s3 = stack.pop();
+                stack.push("0");
+                stack.push(s2.substring(1));
+                stack.push(s3);
+                addNext = false;
+            }
+
+            stack.removeFirst();
+            do {
+                var t = stack.removeLast();
+                sb.append(t);
+            } while (!stack.isEmpty());
+            return new Number(sb.toString());
+        }
+    }
 }
--- day18/src/test/groovy/com/small/advent2021/SnailFishTest.groovy
+++ day18/src/test/groovy/com/small/advent2021/SnailFishTest.groovy
@@ -2,5 +2,59 @@ package com.small.advent2021
 
 import spock.lang.Specification
 
+
 class SnailFishTest extends Specification {
+    private SnailFish snailFish
+
+    def setup() {
+        snailFish = new SnailFish()
+    }
+
+    def "should reduce a fourth number"() {
+        given:
+        SnailFish.Number n1 = new SnailFish.Number("[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]")
+        SnailFish.Number n2 = new SnailFish.Number("[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]")
+
+
+        expect:
+        n2 == n1.reduce()
+    }
+    def "should reduce a third number"() {
+        given:
+        SnailFish.Number n1 = new SnailFish.Number("[[6,[5,[4,[3,2]]]],1]")
+        SnailFish.Number n2 = new SnailFish.Number("[[6,[5,[7,0]]],3]")
+
+
+        expect:
+        n2 == n1.reduce()
+    }
+    def "should reduce a second number"() {
+        given:
+        SnailFish.Number n1 = new SnailFish.Number("[7,[6,[5,[4,[3,2]]]]]")
+        SnailFish.Number n2 = new SnailFish.Number("[7,[6,[5,[7,0]]]]")
+
+
+        expect:
+        n2 == n1.reduce()
+    }
+    def "should reduce a number"() {
+        given:
+        SnailFish.Number n1 = new SnailFish.Number("[[[[[9,8],1],2],3],4]")
+        SnailFish.Number n2 = new SnailFish.Number("[[[[0,9],2],3],4]")
+
+
+        expect:
+        n2 == n1.reduce()
+    }
+    def "should add 2 numbers"() {
+        given:
+        SnailFish.Number n1 = new SnailFish.Number("[1,2]")
+        SnailFish.Number n2 = new SnailFish.Number("[[3,4],5]")
+
+        SnailFish.Number n3 = new SnailFish.Number("[[1,2],[[3,4],5]]")
+
+
+        expect:
+        n3 == n1.add(n2)
+    }
 }
